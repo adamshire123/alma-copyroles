104688cf2e135227d5a8818cd6cce69ec19c495a
Adam Shire <shirea@mit.edu>
2024-09-27 18:46:22 -0400

    add doc strings

--- copy-roles.py
+++ copy-roles.py
@@ -22,29 +22,34 @@ headers = {
     "Content-Type": "application/json"
 }
 def get_alma_configuration() -> dict:
+    """Retrieve general configuration details. 
+    
+    Includes an indication of which environment (production or sandbox) the API key is for."""
     url = base_url + config_endpoint + "general"
     response = requests.get(url=url, headers=headers)
     response.raise_for_status
     return response.json()
 
 def get_alma_user(user) -> dict:
+    """get an alma user object"""
     url = base_url + users_endpoint + requests.utils.quote(user)
     response = requests.get(url, headers=headers)
     response.raise_for_status()
     return response.json()
 
 def get_user_roles(user) -> list:
+    """"return a list of roles of an alma user"""
     roles = user.get('user_role')
     return roles
 
 def print_user_roles(roles):
-
+    """Print a nicely formatted table of alma user roles"""
     roles_tab = [{"status": role['status']['desc'], "role_type": role['role_type']['desc'], "scope": role['scope'].get('desc')} for role in roles]
     print(tabulate(roles_tab, headers='keys'))
 
 def update_roles(roles, user):
-    """replace roles of USER with ROLES. Returns the updated user object"""
-    user['user_role'] = roles
+    """Updates the given user in Alma with the supplied roles"""
+    user['user_role'] = roles # the list of roles in the user object is called 'user_role' (yes, singular)
     url = base_url + users_endpoint + requests.utils.quote(user['primary_id'])
     response = requests.put(url, headers=headers, json=user)
     response.raise_for_status()    
