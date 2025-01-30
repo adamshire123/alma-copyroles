3feaed4bb92b1164143bcb03f46d6338d2cec5cc
Adam Shire <adamshire123@gmail.com>
2025-01-29 21:16:15 -0500

    add copy_user command
    - adds tests for cli
    
    add tests for exception handling
    
    add copy_user instructions
    
    update readme

--- copyroles/cli.py
+++ copyroles/cli.py
@@ -7,54 +7,47 @@ from tabulate import tabulate
 from copyroles.alma import AlmaClient
 
 
-@click.command()
+@click.group()
+def cli() -> None:
+    """CLI for CopyRoles."""
+
+
+@cli.command()
 @click.argument("source_username", type=str)
 @click.argument("target_username", type=str)
 @click.option(
-    "--source-env",
+    "-e",
+    "--environment",
     type=click.Choice(["prod", "sandbox"], case_sensitive=False),
     required=True,
-    help="Environment of the source user (prod or sandbox).",
+    help="The Alma environment(prod or sandbox) within which the roles will be copied.",
 )
-@click.option(
-    "--target-env",
-    type=click.Choice(["prod", "sandbox"], case_sensitive=False),
-    required=True,
-    help="Environment of the target user (prod or sandbox).",
-)
-def main(
-    source_username: str, target_username: str, source_env: str, target_env: str
+def copy_roles(
+    source_username: str,
+    target_username: str,
+    environment: str,
 ) -> None:
-    if source_env == "sandbox" and target_env == "prod":
-        message = "Cannot copy roles from sandbox to prod environment."
-        raise click.UsageError(message)
     alma_api_keys = {}
     alma_api_keys["prod"] = os.environ["PROD_ALMA_API_KEY"]
     alma_api_keys["sandbox"] = os.environ["SANDBOX_ALMA_API_KEY"]
-    try:
-        source_alma_client = AlmaClient(alma_api_keys[source_env])
-        target_alma_client = AlmaClient(alma_api_keys[target_env])
-    except KeyError as e:
-        click.echo(f"Error creating alma client: {e}")
-        return
+    alma_client = AlmaClient(alma_api_keys[environment])
     job_summary = (
         f"Source: \033[1m{source_username.upper()}\033[0m - "
-        f"\033[1m{target_alma_client.alma_environment.upper()}\033[0m\n"
+        f"\033[1m{alma_client.alma_environment.upper()}\033[0m\n"
         f"Target: \033[1m{target_username.upper()}\033[0m - "
-        f"\033[1m{target_alma_client.alma_environment.upper()}\033[0m\n"
+        f"\033[1m{alma_client.alma_environment.upper()}\033[0m\n"
     )
     click.echo("\n-- Copy Alma Roles --\n" + job_summary)
 
     if click.confirm("Continue and review roles?", default=False, abort=False):
         try:
-            source_user = source_alma_client.get_alma_user(source_username)
-            target_user = target_alma_client.get_alma_user(target_username)
-            source_roles: list = source_user["user_role"]
+            source_user = alma_client.get_alma_user(source_username)
+            target_user = alma_client.get_alma_user(target_username)
         except HTTPError as e:
-            click.echo(f"Error getting users: {e}")
-            return
+            error_message = f"Could not get user - {e.response.text}"
+            raise click.ClickException(error_message) from e
         if click.confirm("review roles?"):
-            click.echo(print_user_roles(source_roles))
+            click.echo(print_user_roles(source_user["user_role"]))
         do_copy = click.prompt(
             "\nCopy these Alma roles?\n"
             + job_summary
@@ -63,10 +56,62 @@ def main(
             show_choices=False,
         )
         if do_copy == "copy-roles":
-            target_alma_client.update_alma_roles(source_roles, target_user)
-            click.echo("Roles copied.")
+            try:
+                alma_client.update_alma_roles(source_user["user_role"], target_user)
+                click.echo("Roles copied.")
+            except HTTPError as e:
+                error_message = f"Could not update roles - {e.response.text}"
+                raise click.ClickException(error_message) from e
         else:
             click.echo("Copy operation cancelled.")
+
+
+@click.argument("username", type=str)
+@click.option(
+    "--source-env",
+    type=click.Choice(["prod", "sandbox"], case_sensitive=False),
+    required=True,
+    default="prod",
+    help="Environment of the source user (prod or sandbox).",
+)
+@click.option(
+    "--target-env",
+    type=click.Choice(["prod", "sandbox"], case_sensitive=False),
+    required=True,
+    default="sandbox",
+    help="Environment of the target user (prod or sandbox).",
+)
+@cli.command()
+def copy_user(username: str, source_env: str, target_env: str) -> None:
+    if source_env == "sandbox" and target_env == "prod":
+        message = "Cannot copy users from sandbox to prod environment."
+        raise click.UsageError(message)
+    click.echo("copy user")
+    alma_api_keys = {}
+    alma_api_keys["prod"] = os.environ["PROD_ALMA_API_KEY"]
+    alma_api_keys["sandbox"] = os.environ["SANDBOX_ALMA_API_KEY"]
+    source_alma_client = AlmaClient(alma_api_keys[source_env])
+    target_alma_client = AlmaClient(alma_api_keys[target_env])
+    job_summary = (
+        f"Source: \033[1m{username.upper()}\033[0m - "
+        f"\033[1m{source_alma_client.alma_environment.upper()}\033[0m\n"
+        f"Target: \033[1m{username.upper()}\033[0m - "
+        f"\033[1m{target_alma_client.alma_environment.upper()}\033[0m\n"
+    )
+    click.echo("\n-- Copy Alma User --\n" + job_summary)
+
+    if click.confirm("Continue?", default=False, abort=False):
+        try:
+            source_user = source_alma_client.get_alma_user(username)
+        except HTTPError as e:
+            error_message = f"Could not get user - {e.response.text}"
+            raise click.ClickException(error_message) from e
+        try:
+            target_alma_client.create_alma_user(source_user)
+            click.echo("User copied.")
+        except HTTPError as e:
+            error_message = f"Could not create user - {e.response.text}"
+            raise click.ClickException(error_message) from e
 
 
 def print_user_roles(roles: list) -> str:
